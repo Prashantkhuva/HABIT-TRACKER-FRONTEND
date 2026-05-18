3fd7389c488837951ac43231d493c9039dba17c3
Prashantkhuva <work.prashantkhuva@gmail.com>
2026-05-18 18:03:08 +0530

    show password button added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,6 +6,10 @@ function Input(
 ) {
   const id = useId();
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = type === "password";
+
   return (
     <div className="w-full">
       {label && (
@@ -17,14 +21,28 @@ function Input(
         </label>
       )}
 
-      <input
-        id={id}
-        ref={ref}
-        type={type}
-        placeholder={placeholder}
-        className={`w-full rounded-full border border-gray-300 dark:border-[#2A2A2A] bg-gray-200 dark:bg-[#1D1B20] px-5 py-4 text-sm text-[#1A1A1A] dark:text-[#E6E1E5] outline-none transition-all duration-300 placeholder:text-gray-400/50 dark:placeholder:text-[#938F99]/50 focus:border-[#1A1A1A] dark:focus:border-[#E6E1E5] focus:ring-1 focus:ring-[#1A1A1A] dark:focus:ring-[#E6E1E5] focus:-translate-y-0.5 ${className}`}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          id={id}
+          ref={ref}
+          type={isPassword ? (showPassword ? "text" : "password") : type}
+          placeholder={placeholder}
+          className={`w-full rounded-full border border-gray-300 dark:border-[#2A2A2A] bg-gray-200 dark:bg-[#1D1B20] px-5 py-4 ${
+            isPassword ? "pr-14" : ""
+          } text-sm text-[#1A1A1A] dark:text-[#E6E1E5] outline-none transition-all duration-300 placeholder:text-gray-400/50 dark:placeholder:text-[#938F99]/50 focus:border-[#1A1A1A] dark:focus:border-[#E6E1E5] focus:ring-1 focus:ring-[#1A1A1A] dark:focus:ring-[#E6E1E5] focus:-translate-y-0.5 ${className}`}
+          {...props}
+        />
+
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-5 top-1/2 -translate-y-1/2 text-gray-400 dark:text-[#938F99]"
+          >
+            {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
